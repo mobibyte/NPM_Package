c1ff2928971bfb23f4f53b9df23ca1fdbfef82ce
Pepsi <epicendless122@gmail.com>
2024-11-14 18:11:07 -0600

    feat Added files

---
+++ index.js
@@ -0,0 +1,33 @@
+// Simple addition
+function addThis(a, b) {
+    var val1 = a;
+    var val2 = b;
+
+    return val1 + val2;
+}
+
+// Simple subtraction
+function subtractThis(a, b) {
+    var val1 = a;
+    var val2 = b;
+
+    return val1 - val2;
+}
+
+// Simple Multiplication
+function multiplyThis(a, b) {
+    var val1 = a;
+    var val2 = b;
+
+    return val1 * val2;
+}
+
+// Simple Division
+function divideThis(a, b) {
+    var val1 = a;
+    var val2 = b;
+
+    return val1 / val2;
+}
+
+module.exports = {addThis, subtractThis, multiplyThis, divideThis}
